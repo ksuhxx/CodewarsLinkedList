7b7cafcb7fea234f588c656b7473417b9645b819
Ksenia Nimas <nimas.ksenja@gmail.com>
2026-03-27 14:16:13 +0200

    Додано рішення задачі parse a linked list from a string

---
+++ Parse_a_linked_list_from_a_string/solution.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(list_repr: str):
+    if list_repr == "None":
+        return None
+
+    values = list_repr.split(" -> ")[:-1]
+
+    head = Node(int(values[0]))
+    current = head
+
+    for value in values[1:]:
+        current.next = Node(int(value))
+        current = current.next
+
+    return head
